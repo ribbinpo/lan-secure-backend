470be2992d0668dfcaa88c19cc8391cc6c525090
ribbinpo <ribbinpo@gmail.com>
2022-07-15 16:10:29 +0700

    Change to FastAPI server

---
+++ main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+import aiofiles
+# import shutil;
+
+app = FastAPI()
+
+@app.post("/upload-file")
+async def create_upload_file(file: UploadFile):
+    print("filename = ", file.filename) # getting filename
+    destination_file_path = "./uploads/"+file.filename # location to store file
+    async with aiofiles.open(destination_file_path, 'wb') as out_file:
+        while content := await file.read(1024):  # async read file chunk
+            await out_file.write(content)  # async write file chunk
+    return {"Result": "OK"}
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
