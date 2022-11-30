ef4577b283dd25fbaca5f668e3f4deb28c3dd3bb
ธีระวุฒิ แซ่ซิ้ม <ribbinpo@Macintosh.local>
2022-11-30 19:47:39 +0700

    Add CORS origin

--- core/config.py
+++ core/config.py
@@ -25,5 +25,7 @@ settings = Settings()
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
-    os.getenv("FRONTEND_URL")
+    os.getenv("FRONTEND_URL"),
+    "https://symphonious-madeleine-aa3f5d.netlify.app"
+    "*"
 ]
